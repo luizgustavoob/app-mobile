63ed8d565941d18ce5944f520659e0d5809d59dc
luiz.gustavo <luizgustavobourscheit@gmail.com>
2017-02-05 23:10:12 -0200

    Sobreescrever métodos de sincronização

--- paraondeirapp/app/src/main/java/br/com/paraondeirapp/servidor/sincronizacao/impl/SincronizacaoFirebase.java
+++ paraondeirapp/app/src/main/java/br/com/paraondeirapp/servidor/sincronizacao/impl/SincronizacaoFirebase.java
@@ -2,13 +2,17 @@ package br.com.paraondeirapp.servidor.sincronizacao.impl;
 
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.util.Log;
 
+import java.io.InputStream;
 import java.lang.reflect.Type;
+import java.util.Calendar;
 import java.util.List;
 
 import br.com.paraondeirapp.entity.Firebase;
 import br.com.paraondeirapp.interfaces.IConstantesServidorSinc;
 import br.com.paraondeirapp.servidor.sincronizacao.Sincronizacao;
+import br.com.paraondeirapp.utils.ConexaoUtils;
 
 public class SincronizacaoFirebase extends Sincronizacao<Firebase> {
 
@@ -29,5 +33,26 @@ public class SincronizacaoFirebase extends Sincronizacao<Firebase> {
     @Override
     protected void salvarSincronizacao(List<Firebase> lista) {
 
+    }
+
+    @Override
+    protected List<Firebase> post() throws Exception {
+        try {
+            InputStream stream = ConexaoUtils.post(getLinkSincronizacao(), getJsonPost());
+        } catch (Exception ex){
+            throw new Exception(ex.getMessage());
+        }
+        return null;
+    }
+
+    @Override
+    public void sincronizar() throws Exception {
+        Log.i("Sincronizacao.sincronizar()", Calendar.getInstance() + " >> Sincronizando etapa " + getEtapa());
+        atualizarProgressBar();
+        try {
+            post();
+        } catch (Exception ex){
+            throw new Exception("Erro na etapa " + getEtapa() + ". " + ex.getMessage());
+        }
     }
 }
